09da1c74e698ad931c69daf84235df1dcdb9bf8d
Behzod <behzodtuit@gmail.com>
2022-10-24 22:34:05 +0500

    auth

--- class/urls.py
+++ class/urls.py
@@ -2,7 +2,8 @@ from django.urls import path,include
 from .views  import *
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register('class',ClassRoomViewset)
+router.register('class',ClassRoomViewset),
+router.register('davomat',DavomatViewset)
 urlpatterns = [
     path('',include(router.urls)),
     path('getclasscount/',GetClassRoomCount.as_view())
